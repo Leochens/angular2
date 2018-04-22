f6dd80040f091104604ddc840d7abca7d718fd42
Leochens <18332518328@163.com>
2018-04-22 14:10:52 +0800

    英雄列表

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,6 +12,7 @@ export class HeroesComponent implements OnInit {
       id: 1,
       name:"WindStorm"
   };
+  heroes=HEROES;
   constructor() { }
 
   ngOnInit() {
